d75950597e688d91e0b0afcc498972c44ed23fe1
Gattusoangers <2289860885@qq.com>
2018-04-08 21:02:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hasee/easy2/FragmentTwo.kt
@@ -0,0 +1,17 @@
+package com.example.hasee.easy2
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lin.coursetwo.R;
+/**
+ * Created by Hasee on 2018/4/8.
+ */
+class FragmentTwo : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle): View? {
+        return inflater.inflate(R.layout.fragment_two, container, false)
+    }
+}
